6b5a0979cda253902610e3afb07fadfdd408f35e
felix_bonnier <felix_bonnier@yahoo.com>
2019-03-13 19:41:53 +0100

    adding all variable exercises for name.rb

---
+++ name.rb
@@ -0,0 +1,19 @@
+#Variables: 1 - name.rb
+puts "State your name"
+name = gets.chomp
+
+
+#Variables: 3 - name.rb
+10.times do |x|
+puts "Greetings "+name
+end
+
+
+#Variables: 4 - name.rb
+puts "State your first name"
+firstname = gets.chomp
+
+puts "State your last name"
+lastname = gets.chomp
+
+puts firstname+" "+lastname
