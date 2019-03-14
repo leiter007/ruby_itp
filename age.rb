db6d1f039d54c8f2bcbc9b5968ad69c86b16c678
felix_bonnier <felix_bonnier@yahoo.com>
2019-03-14 22:33:54 +0100

    adding the age file with variable exercise

---
+++ age.rb
@@ -0,0 +1,14 @@
+#Variables: 2 - age.rb
+puts "How old are you?"
+
+age = gets.chomp.to_i
+
+age_1 = age + 10
+age_2 = age + 20
+age_3 = age + 30
+age_4 = age + 40
+
+puts "In 10 years you will be #{age_1} years old"
+puts "In 20 years you will be #{age_2} years old"
+puts "In 30 years you will be #{age_3} years old"
+puts "In 40 years you will be #{age_4} years old"
