e7276f6c53bd450e6060f601a03d4b337750a558
felix_bonnier <felix_bonnier@yahoo.com>
2019-03-14 22:38:35 +0100

    adding methods exercises

---
+++ methods.rb
@@ -0,0 +1,26 @@
+#Methods: 1
+def greeting(name)
+    puts name
+end
+  
+greeting("Hello there!")
+
+
+
+#Methods: 2
+def multiply(a, b)
+    a * b
+end
+
+puts multiply(5, 3)
+
+#Methods: 4
+def scream(words)
+    puts words + "!!!!"
+end
+
+scream("Yippeee")
+
+
+  
+  
