53ff28271b4f55bd7e85d18235c2f4a5b96b6e68
felix_bonnier <felix_bonnier@yahoo.com>
2019-03-14 22:37:42 +0100

    adding variables exercises

---
+++ variables.rb
@@ -0,0 +1,42 @@
+#Variables: 1 - name.rb
+puts "State your name"
+name = gets.chomp
+
+#Variables: 3 - name.rb
+10.times do |x|
+puts "Greetings "+name
+end
+
+#Variables: 4 - name.rb
+puts "State your first name"
+firstname = gets.chomp
+
+puts "State your last name"
+lastname = gets.chomp
+
+puts firstname+" "+lastname
+
+
+
+#Variables: 2 - age.rb
+puts "How old are you?"
+age = gets.chomp
+
+newage = age.chomp + i
+
+Until i = 40
+    puts "In #{i} years you will be #{newage} years old"
+    i = i + 10
+end
+
+
+#Variables: 5
+y = 0
+3.times do
+    y += 1
+    x = y
+end
+puts x
+
+
+
